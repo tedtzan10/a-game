73fe965473de768dc5fe97489efccb9e0279f451
tedtzan10 <teddytzanos@gmail.com>
2026-02-13 23:16:22 +0000

    new file:   a_game/src/main/java/graphics/Window 	modified:   a_game/src/main/java/main/GameLoop.java 	new file:   a_game/target/classes/graphics/Window 	modified:   a_game/target/classes/main/GameLoop.class

--- a_game/src/main/java/main/GameLoop.java
+++ a_game/src/main/java/main/GameLoop.java
@@ -1,10 +1,28 @@
 package main;
 
-public class GameLoop {
+import graphics.Window;
 
+public class GameLoop implements Runnable {
+
+    private Thread gameThread;
+
+    private void start() {
+        gameThread = new Thread("fart");
+        gameThread.start();
+    }
     public static void main(String[] args) {
-        System.out.println("Game loop started!");
-        
+        new GameLoop().start();
+        System.out.println("Hello world");
+    }
+
+    @Override
+    public void run() {
+        /* 
+        while () {
+            // Game loop logic would go here
+            
+        }
+        */
     }
     
 }
